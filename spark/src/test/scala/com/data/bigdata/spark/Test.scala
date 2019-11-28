adc412cf648699a2e0a25aa6b9c0292e1a3aa7a9
asus <asus@windows10.microdone.cn>
2019-11-28 15:00:25 +0800

    spark测试

--- spark/src/test/scala/com/data/bigdata/spark/Test.scala
+++ spark/src/test/scala/com/data/bigdata/spark/Test.scala
@@ -1,7 +1,17 @@
 package com.data.bigdata.spark
 
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
 object Test {
   def main(args: Array[String]): Unit = {
-    println("hello scala")
+    
+     val spark = SparkSession.builder().appName("Spark-SQL").master("local[2]").getOrCreate()
+     val df = spark.read.json("E:\\student.json")
+     df.show()
+     //df.show(100)
+
+     // 建议在进行 spark SQL 编程前导入下面的隐式转换，因为 DataFrames 和 dataSets 中很多操作都依赖了隐式转换
+     import spark.implicits._
   }
 }
